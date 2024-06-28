8c8f84102ac928583f9a50639c248f9afc40617a
BrianCheng-TheLegend <briancheng1229@gmail.com>
2024-06-28 13:37:39 +0800

    Create functions to insert elements to queue
    Implemented function to insert an element at the queue.
    Allocates memory for the new element and duplicates the input string.
    Ensures proper cleanup if memory allocation fails.
    Adds the new element to the head or tail of the queue.

--- queue.c
+++ queue.c
@@ -37,12 +37,34 @@ void q_free(struct list_head *head)
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+    element_t *element = malloc(sizeof(element_t));
+    if (!element)
+        return false;
+    element->value = strdup(s);
+    if (!element->value) {
+        free(element);
+        return false;
+    }
+    list_add(&element->list, head);
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (!head)
+        return false;
+    element_t *element = malloc(sizeof(element_t));
+    if (!element)
+        return false;
+    element->value = strdup(s);
+    if (!element->value) {
+        free(element);
+        return false;
+    }
+    list_add_tail(&element->list, head);
     return true;
 }
 
